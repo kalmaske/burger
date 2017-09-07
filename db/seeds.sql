f71d9f1fdd98e5f15e5790aedc81cc1d65d883d5
kalmaske <kalmaske@uncc.edu>
2017-09-06 20:51:31 -0400

    database

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name) VALUES ('Jersey Burger');
+INSERT INTO burgers(burger_name) VALUES ('Grilled Salmon Burger');
+INSERT INTO burgers(burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers(burger_name) VALUES ('Chicken and Mushroom Burger');
+INSERT INTO burgers(burger_name) VALUES ('Plain Traditional Hamburger');
