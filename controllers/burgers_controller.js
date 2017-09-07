a03dc3c0c1d9efccb8850081ab6d8fa3dda48623
kalmaske <kalmaske@uncc.edu>
2017-09-07 13:08:51 -0400

    fix

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,7 +17,7 @@ router.get('/burgers', function(req, res){
 });
 
 router.post('/burgers/create', function(req, res){
-	burgers.create("burgers",['burger_name','createdAt'], [req.body.b_name,new Date.now()], function(data){
+	burgers.create("burgers",['burger_name','createdAt'], [req.body.b_name, Date.now()], function(data){
 		res.redirect('/burgers')
 	});
 });
